41f992419d27e7727efa97416ff67bdf41b1a867
samueladdisu <samueladdisu7@gmail.com>
2022-12-21 19:34:58 +0300

    req

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,3 @@
-export default function getResponseFromAPI(){
-  return new Promise(function(resolve, reject) {})
+export default function getResponseFromAPI() {
+  return new Promise(function (resolve, reject) {});
 }

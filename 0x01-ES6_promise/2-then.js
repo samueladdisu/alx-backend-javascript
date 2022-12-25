87fa83a35dfafc9e7c3f05a7c0999ec9e7873163
samueladdisu <samueladdisu7@gmail.com>
2022-12-25 15:41:20 +0300

    promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,5 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'Success' };
-  }).catch((error) => {
-    console.log(error);
-    return new Error({});
-  });
+  }).catch(() => { Error({}); });
 }

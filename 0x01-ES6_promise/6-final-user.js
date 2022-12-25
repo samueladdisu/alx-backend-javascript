92c694f70b3ee952056523535b20f893c68d5d19
samueladdisu <samueladdisu7@gmail.com>
2022-12-25 17:49:26 +0300

    fiste_copilot2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const photo = uploadPhoto(fileName);
-  const user = signUpUser(firstName, lastName);
-  return Promise.allSettled([photo, user]).then((values) => {
-    values.map((o) => ({
-      status: o.status,
-      value: o.status === 'fulfilled' ? o.value : o.reason,
-    }));
-  });
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }

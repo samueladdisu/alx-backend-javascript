58d02469d5da34826292c1b7ef1eb381a274b272
samueladdisu <samueladdisu7@gmail.com>
2022-12-25 16:23:16 +0300

    reject_promises

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -7,6 +7,10 @@
 
 // handleProfileSignup();
 
-import signUpUser from "./4-user-promise";
+// import signUpUser from "./4-user-promise";
 
-console.log(signUpUser("Bob", "Dylan"));
+// console.log(signUpUser("Bob", "Dylan"));
+
+import uploadPhoto from './5-photo-reject';
+
+console.log(uploadPhoto('guillaume.jpg'));

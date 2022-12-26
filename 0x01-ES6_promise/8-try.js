af0e0b5f4fc61307392385e7cb648429ce570b3e
samueladdisu <samueladdisu7@gmail.com>
2022-12-26 19:18:24 +0300

    try1

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -3,8 +3,8 @@ export default function divideFunction(numerator, denominator) {
     if (denominator === 0) {
       throw new Error('cannot divide by 0');
     }
+    return numerator / denominator;
   } catch (error) {
     console.log(error);
   }
-  return numerator / denominator;
 }

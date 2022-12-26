f9ccbeabace3ec96fbdf24357c65b37bb76bcddc
samueladdisu <samueladdisu7@gmail.com>
2022-12-26 19:17:21 +0300

    try

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,10 @@
+export default function divideFunction(numerator, denominator) {
+  try {
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return numerator / denominator;
+}
--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -19,26 +19,31 @@
 
 // console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
 
-import loadBalancer from "./7-load_balancer";
+// import loadBalancer from "./7-load_balancer";
 
-const ukSuccess = 'Downloading from UK is faster';
-const frSuccess = 'Downloading from FR is faster';
+// const ukSuccess = 'Downloading from UK is faster';
+// const frSuccess = 'Downloading from FR is faster';
 
-const promiseUK = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 100, ukSuccess);
-});
+// const promiseUK = new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 100, ukSuccess);
+// });
 
-const promiseUKSlow = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 400, ukSuccess);
-});
+// const promiseUKSlow = new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 400, ukSuccess);
+// });
 
-const promiseFR = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 200, frSuccess);
-});
+// const promiseFR = new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 200, frSuccess);
+// });
 
-const test = async () => {
-    console.log(await loadBalancer(promiseUK, promiseFR));
-    console.log(await loadBalancer(promiseUKSlow, promiseFR));
-}
+// const test = async () => {
+//     console.log(await loadBalancer(promiseUK, promiseFR));
+//     console.log(await loadBalancer(promiseUKSlow, promiseFR));
+// }
 
-test();
+// test();
+
+import divideFunction from './8-try';
+
+console.log(divideFunction(10, 2));
+console.log(divideFunction(10, 0));

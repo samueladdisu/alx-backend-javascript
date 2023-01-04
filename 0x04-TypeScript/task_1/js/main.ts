a12ee607059d0fe8edf4d26e7a998149913cdf48
samueladdisu <samueladdisu7@gmail.com>
2023-01-04 20:33:19 +0300

    print-teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,12 +11,6 @@ interface Directors extends Teacher {
   numberOfReports: number;
 };
 
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);
+function printTeacher(firstName: string, lastName: string) {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
